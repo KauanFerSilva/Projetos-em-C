4f89e8d721f878bc5c9cf51766fe6128e73aba88
Kauan Ferreira <kauanferreira70771@gmail.com>
2026-09-23 22:24:27 -0300

    Add main function to calculate total students

---
+++ projeto.c
@@ -0,0 +1,18 @@
+int main(){
+    //variaveis
+    int meninos, meninas, soma;
+
+    //entrada de dados
+    printf("Digite o numero de meninos da sala: ");
+    scanf("%d", &meninos);
+    printf("Digite o numero de meninas da sala: ");
+    scanf("%d", &meninas);
+
+    //processameto
+    soma = meninos + meninas;
+    printf("O total de alunos da sala e: %d", soma);
+
+    //final do código
+    fflush (stdin);
+    return 0;
+}
